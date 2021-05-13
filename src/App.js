d544d522c3f63381856d29601f39e63d981a3c7b
peachgrace <76766539+peachgrace@users.noreply.github.com>
2021-05-14 08:04:24 +0900

    5.14
    createRequired is not function error....

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from "axios";
+import Movie from "./Movie";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+
+  getMovies = async () => { //비동기
+    const { //(object).data.data.movies
+      data: {
+        data: { movies }
+      }
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false }); //"map~"
+  };
+
+  componentDidMount() { //생명주기함수; Component render 완료후 실행.
+    this.getMovies();
+  }
+  render() {
+    const { isLoading, movies } = this.state; //app.state.isLoading 이 render()이하에에서의 isLoading과 같다...
+    return (<div>
+      {isLoading ? "Loading..." : movies.map( movie => (
+
+      <Movie key={movie.id} id={movie.id} year={movie.year} title={movie.title}  summary={movie.summary} 
+      poster={movie.medium_cover_image} />)
+    )}
+  </div>);
+  }
 }
 
 export default App;
